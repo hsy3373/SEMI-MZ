a9a0af0f7a10b14fb3bddc1d5e29b77bb659c6bc
hsy3373 <hsy3373@naver.com>
2023-03-22 13:35:39 +0900

    3.22

--- SEMI_MZ/src/mz/AjaxChatting.java
+++ SEMI_MZ/src/mz/AjaxChatting.java
@@ -34,8 +34,12 @@ public class AjaxChatting extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String content = request.getParameter("content");
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		int userNo = ((Member)request.getSession().getAttribute("loginUser")).getUserNo();
+		int result = new BoardService().insertReply(content, bno, userNo);
+		
+		response.getWriter().print(result);
 	}
 
 }
