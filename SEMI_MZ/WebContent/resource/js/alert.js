6e8794459ce56d510d0fc760fe9a84050f4ac32a
nohjieui <114555765+nohjieui@users.noreply.github.com>
2023-03-26 22:15:30 +0900

    친구가 쓴 방명록 리스트 계속

--- SEMI_MZ/WebContent/resource/js/alert.js
+++ SEMI_MZ/WebContent/resource/js/alert.js
@@ -9,11 +9,9 @@ let alertOverlay = document.querySelector('.alert-overlay');
 let alertToggle = document.querySelector('.alert-toggle');
 alertToggle.addEventListener('click', openAlert);
 
+// openAlert() : 외부 버튼 클릭시 alert 창 실행시키기 위한 함수
 function openAlert() {
-  // 모달을 닫는 표시기를 듣습니다.
-  alertOverlay.addEventListener('click', closeAlert);
-
-  // 취소버튼
+  // 취소버튼 클릭시 alert 창 닫힘
   let cancelBtn = alert.querySelector('.alert-cancel');
   cancelBtn.addEventListener('click', closeAlert);
 
@@ -23,40 +21,37 @@ function openAlert() {
 
 }
 
+// closeAlert() : 모달과 오버레이 숨기기
 export function closeAlert() {
-  // 모달과 오버레이 숨기기
   alert.style.display = 'none';
   alertOverlay.style.display = 'none';
 }
 
-
-
-
 export function alertAddClass(str){
-  //  okBtn 안의 click 이벤트 함수가 만약 있다면 해당 이벤트 삭제
-  //~~~
+  // id="alert-text" 요소에 class 속성을 추가하고 속성의 값은 변수명이 str인 값으로 한다.
   $("#alert-text").attr("class" , str);
   openAlert();
 }
 
 
-//--------------------------------------------------------------------
+// -------------------------------------- 추가 및 생성해야할 파일 --------------------------------------
 
-//자기 파일용 함수작성
+// 자기 파일용 함수작성
 function testF(){
-  //숨겨놓은 div class값 조회해 와서 그에따라 함수 실행됨
-    let str = $("#alert-text").attr("class");
+  // id="alert-text"의 class 속성의 값을 가져와 str 변수에 담아둔다.
+  let str = $("#alert-text").attr("class");
 
-    switch(str){
-      case "update" : 
+  switch(str){
+    case "update" : 
+      // 실행시 구현할 코드 작성
+      
+      break;
 
-          break;
+    case "delete" : 
 
-      case "delete" : 
-
-          break;
-  }
-  closeAlert();
+      break;
+    }
+    closeAlert();
 }
 
 // 위에서 자기용으로 만든 함수를 이벤트로 넣어주기
@@ -64,11 +59,59 @@ document.querySelector("#alert-ok").addEventListener('click', testF);
 
 // 각 본인이 만든 버튼을 누르면 어떤 class명을 줄건지 각각 설정
 $("#test1").click(function(){
-  $("#alert-text").html("테스트중");
+  $("#alert-text").html("수정 테스트중");
   alertAddClass("update");
 });
 $("#test2").click(function(){
-  $("#alert-text").html("테스트중2");
+  $("#alert-text").html("삭제 테스트중");
   alertAddClass("delete");
 })
+ 
+  
+
+
+// -------------------------- 마이룸(지의) --------------------------
+function myroomAlert(){
+
+  let str = $("#alert-text").attr("class");
+
+  switch(str){
+    case "board-delete" :
+    
+    // 
+
+
+    break;
+
+    case "closet-buy" :
+     
+    // 
+
+    break;
+
+    case "board-send-delete" :
+     
+    // 
+
+    break;
+  }
+  closeAlert();
+}
+
+// alert 창에서 확인 버튼 클릭 이벤트 추가
+document.querySelector("#alert-ok").addEventListener('click', myroomAlert);
+
+$("#board-delete").click(function(){
+  $("#alert-text").html("삭제하시겠습니까?");
+  alertAddClass("board-delete");
+})
+$("#closet-buy").click(function(){
+  $("#alert-text").html("구입하시겠습니까?");
+  alertAddClass("closet-buy");
+})
+$("#board-send-delete").click(function(){
+  $("#alert-text").html("방명록을 삭제하시겠습니까?");
+  alertAddClass("board-send-delete");
+})
+
 
--- SEMI_MZ/WebContent/resource/js/myroom.js
+++ SEMI_MZ/WebContent/resource/js/myroom.js
@@ -3,7 +3,7 @@
  *	마이룸  JS
  */
 $(function(){
-	/* -------------------- 방명록 -------------------- */
+	/* ==================================== 방명록 ==================================== */
     /* 방명록 모달창 띄우기 */
     $(".icon-tree").click(function(e){
         $(".board-wrap").show();
@@ -11,6 +11,8 @@ $(function(){
         $(".board-list").show();
         $(".board-detail").hide();
         $(".board-write").hide();
+        $(".board-send-list").hide();
+        $(".board-send-detail").hide();
     })
     
     /* 방명록 상세페이지 모달창 띄우기 */
@@ -29,13 +31,13 @@ $(function(){
     $("body").on("click", function(e) {
         console.log(e.target);
         if(e.target.className == 'x-btn' || e.target.className == 'board-wrap'){
-            $(".board-wrap").hide(); // 방명록 모달 닫힘
+            $(".board-wrap").hide(); // 방명록 모달 닫힘 
             $(".closet-wrap").hide(); // 옷장 모달 닫힘
         }
     })
 
     /* back버튼 클릭시 방명록리스트 모달창 보여짐 */
-    $(".back-btn").click(function(e){
+    $(".board-detail .back-btn").click(function(e){
         $(".board-list").show();
         $(".board-detail").hide();
         $(".board-write").hide();
@@ -47,9 +49,64 @@ $(function(){
     /* 방명록 작성 이벤트 */
 
 
-	/* -------------------- 옷장 -------------------- */
-	    /* 방명록 모달창 띄우기 */
+	/* ==================================== 옷방 ==================================== */
+	/* 옷방 모달창 띄우기 */
     $(".icon-closet").click(function(e){
         $(".closet-wrap").show();
-    })
+        // $(".closet-modal").show();
+        $.dressClick();
+    });
+    
+    // 옷장 버튼 클릭
+    $(".dress-btn").click(function(){
+        $.dressClick();
+    });
+
+    // 상점 버튼 클릭
+    $(".store-btn").click(function(){
+        $.storeClick();
+    });
+    
+    // 옷장 버튼 클릭 함수 생성
+    $.dressClick = function(){
+        // 1. 상점 버튼 투명하게
+        $(".store-btn").css("opacity", "0.7");
+        $(".dress-btn").css("opacity", "1");
+        
+        // 2. 커서 포인터 없애기
+        $(".dress-btn").css("cursor", "default");
+        $(".store-btn").css("cursor", "pointer");
+        
+        // 3. 구입 버튼(class="closet-buy") -> display: "none";
+        $(".closet-buy").hide();
+        $(".closet-wear").show();
+        
+        // 4. 가격, 보유중 라벨(class="closet-price") -> display: "none";
+        $(".closet-price").hide();
+        
+        // 5. 내가 보유중인 스킨만 표시(DB -> "CHARACTER"테이블 SELECT)
+
+    }
+
+    // 상점 버튼 클릭 함수 생성
+    $.storeClick = function(){
+        // 1. 옷장 버튼 투명하게
+        $(".dress-btn").css("opacity", "0.7");
+        $(".store-btn").css("opacity", "1");
+
+        // 2. 커서 포인터 없애기
+        $(".store-btn").css("cursor", "default");
+        $(".dress-btn").css("cursor", "pointer");
+
+        // 3. 착용 버튼(class="closet-wear") -> display: "none";
+        $(".closet-wear").hide();
+        $(".closet-buy").show();
+
+        // 4. 가격, 보유중 라벨(class="closet-price") -> display: "block";
+        $(".closet-price").show();
+
+        // 5. 내가 보유한 스킨이라면 (class="closet-price")
+
+
+    }
 });
