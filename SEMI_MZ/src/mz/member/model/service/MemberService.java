bef82699fc19e1b0683d4bf878b621adaf54f697
SSHwan22 <riddl5076@gmail.com>
2023-03-27 15:18:35 +0900

    .

--- SEMI_MZ/src/mz/member/model/service/MemberService.java
+++ SEMI_MZ/src/mz/member/model/service/MemberService.java
@@ -1,5 +1,7 @@
 package mz.member.model.service;
 
+import static mz.common.JDBCTemplate.*;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -10,15 +12,14 @@ import mz.member.model.vo.Member;
 public class MemberService {
 	
 	public Member selectMember(String userId) {
-		Connection conn = JDBCTemplate.getConnection();
 		
-		Member m = new MemberDao().selectMember(conn, userId);
+		Connection conn = getConnection();
 		
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return m;
+		Notice n = new NoticeDao().selectNotice(conn, nno);
+		
+		// DML 문이 아니기때문에 트랜젝션 처리 x
+		close(conn);
+		
+		return n;
 	}
 }
